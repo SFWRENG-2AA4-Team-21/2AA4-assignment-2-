6f24ec0430be19c7be7338faa40a81aeb528ec8d
ReanzJeanz <antonj10mcmaster.ca>
2026-03-10 20:54:43 -0400

    added test parser files

---
+++ testing_files/RegexTester.java
@@ -0,0 +1,15 @@
+public class RegexTester {
+    public String parse(String str){
+        if(str.matches("^Roll$")){
+            return "Dice Rolled";
+        }else if(str.matches("^Go$")){
+            return "Movement Enacted\nTurn Ended";
+        }else if(str.matches("^List$")){
+            return "Here's a list of your cards on hand";
+        }else if(str.matches("^Build (settlement|city) (\\d+)$")||str.matches("^Build (road) (\\d+),(\\d+)$")){
+            return "Building your card";
+        }
+        return "Failed Result";
+    }
+
+}

debe5f2ddee39253bc122fafe1440b25eb11082f
Sanjna <sanjnabanda@gmail.com>
2026-03-11 23:16:30 -0400

    Adding comments to demonstrator class

--- src/catan/Demonstrator.java
+++ src/catan/Demonstrator.java
@@ -7,14 +7,18 @@ import java.util.Scanner;
 public class Demonstrator {
 
     public static void main(String[] args) {
+        /**
+         * scanner object used to read user input from the command line 
+         * Supports requirment R2.1
+        */
         Scanner scanner = new Scanner(System.in);
-        System.out.println("choose a mode");
-        System.out.println("1 COMP VS COMP");
-        System.out.println("2 HUMAN VS COMP");
+        System.out.println("choose a mode"); //prompts user to select game mode 
+        System.out.println("1 COMP VS COMP"); //automated simulation of computer players 
+        System.out.println("2 HUMAN VS COMP"); // simulation of human and computer player 
 
-        int move=scanner.nextInt();
+        int move=scanner.nextInt(); //reads user input 
 
-        Game game = new Game(move);
-        game.startGame();
+        Game game = new Game(move); //new game object based selected game mode
+        game.startGame(); //starts the game simulation
     }
 }
